d04278f6fce6a78cc0f1e6b0e479873214b9d047
Pawel Bak <pawelbak300589@gmail.com>
2021-06-14 20:19:29 +0100

    Add simple search bar

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Blank canvas</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar placeholder="Search" />
+      </View>
+      <View style={styles.list}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : "",
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
 });
