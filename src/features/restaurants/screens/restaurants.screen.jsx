f44a7920dba89e2baa9801b076004ec78056dc22
Pawel Bak <pawelbak300589@gmail.com>
2021-06-18 19:43:39 +0100

    Add styled components to the restaurant screen

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,42 +1,38 @@
 import React from "react";
 import {
-  StyleSheet,
-  View,
-  Text,
-  SafeAreaView,
-  Platform,
   StatusBar,
 } from "react-native";
+import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 
 import RestaurantInfoCard from "../components/restaurant-info-card.component";
 
+const SafeArea = styled.SafeAreaView`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+`;
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
+
 const RestaurantsScreen = (props) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Searchbar placeholder="Search" />
-      </View>
-      <View style={styles.list}>
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantListContainer>
         <RestaurantInfoCard />
-      </View>
-    </SafeAreaView>
+      </RestaurantListContainer>
+    </SafeArea>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : "",
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
 
 export default RestaurantsScreen;
