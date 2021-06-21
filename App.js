af9b6896c22102483536820834e81b077dc9594e
Pawel Bak <pawelbak300589@gmail.com>
2021-06-21 19:03:29 +0100

    Add fonts to the app

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  let [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
